aeb45e4caa73e05fb276f100b81f4af3cf80cbe4
ouzezh <ouzezh@163.com>
2021-04-12 20:40:22 +0800

    cache condition

--- SpringbootDemo/src/test/java/com/ozz/cache/service/MyCacheServiceTest.java
+++ SpringbootDemo/src/test/java/com/ozz/cache/service/MyCacheServiceTest.java
@@ -1,7 +1,6 @@
 package com.ozz.cache.service;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +22,6 @@ class MyCacheServiceTest {
     Assert.isTrue(ldt.equals(myCacheService.getData(days, hours)), StringUtils.EMPTY);
   }
 
-  @Test
-  void getDataCheck() {
-    Integer days = 2;
-    String ldt = myCacheService.getDataCheck(days, null);
-    String ldt2 = myCacheService.getDataCheck(days, "2");
-    sleep(5);
-    Assert.isTrue(!ldt.equals(myCacheService.getDataCheck(days, null)), StringUtils.EMPTY);
-    Assert.isTrue(!ldt.equals(myCacheService.getDataCheck(days, Strings.EMPTY)), StringUtils.EMPTY);
-    Assert.isTrue(ldt2.equals(myCacheService.getDataCheck(days, "2")), StringUtils.EMPTY);
-  }
-  
   @Test
   void refreshAndGetData() {
     int days = 1, hours = 3;
